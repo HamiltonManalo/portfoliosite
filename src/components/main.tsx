e35885b9bc23cea903f4d5b9f3a6aefe1390195f
Hamilton <hamiltonm@gmail.com>
2018-11-06 00:45:24 +0000

    no message

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -6,12 +6,12 @@ export default class Main extends React.Component {
     }
     public render() {
         return (
-            <div className="grid">    
+           
                 <div className="page-content"> 
                 <h2>Main</h2>
                 <p> This site is still going through some growing pains.</p>
                 </div> 
-            </div>
+           
         )
     }
 }
